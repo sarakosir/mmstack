edfc7c104d2ca267595e5bab43b8ce28efc628d7
miham <miham@marand.si>
2025-03-24 08:32:37 +0100

    feat: add jsdoc to existing primitives

--- packages/primitives/src/lib/mutable.ts
+++ packages/primitives/src/lib/mutable.ts
@@ -1,65 +1,133 @@
 import {
-    signal,
-    type CreateSignalOptions,
-    type ValueEqualityFn,
-    type WritableSignal,
-  } from '@angular/core';
-  
-  const { is } = Object;
-  
-  export type MutableSignal<T> = WritableSignal<T> & {
-    mutate: WritableSignal<T>['update'];
-    inline: (updater: Parameters<WritableSignal<T>['update']>[0]) => void;
+  signal,
+  type CreateSignalOptions,
+  type ValueEqualityFn,
+  type WritableSignal,
+} from '@angular/core';
+
+const { is } = Object;
+
+/**
+ * A `MutableSignal` is a special type of `WritableSignal` that allows for in-place mutation of its value.
+ * In addition to the standard `set` and `update` methods, it provides a `mutate` method.  This is useful
+ * for performance optimization when dealing with complex objects or arrays, as it avoids unnecessary
+ * object copying.
+ *
+ * @typeParam T - The type of value held by the signal.
+ */
+export type MutableSignal<T> = WritableSignal<T> & {
+  /**
+   * Mutates the signal's value in-place.  This is similar to `update`, but it's optimized for
+   * scenarios where you want to modify the existing object directly rather than creating a new one.
+   *
+   * @param updater - A function that takes the current value as input and modifies it directly.
+   *
+   * @example
+   * const myArray = mutable([1, 2, 3]);
+   * myArray.mutate((arr) => {
+   *  arr.push(4);
+   *  return arr;
+   * }); // myArray() now returns [1, 2, 3, 4]
+   */
+  mutate: WritableSignal<T>['update'];
+
+  /**
+   * Mutates the signal's value in-place, similar to `mutate`, but with a void-returning value in updater
+   * function. This further emphasizes that the mutation is happening inline, improving readability
+   * in some cases.
+   * @param updater - Function to change to the current value
+   * @example
+   * const myObject = mutable({ a: 1, b: 2 });
+   * myObject.inline((obj) => (obj.a = 3)); // myObject() now returns { a: 3, b: 2 }
+   */
+  inline: (updater: Parameters<WritableSignal<T>['update']>[0]) => void;
+};
+
+/**
+ * Creates a `MutableSignal`. This function overloads the standard `signal` function to provide
+ * the additional `mutate` and `inline` methods.
+ *
+ * @typeParam T - The type of value held by the signal.
+ *
+ * @param initial - The initial value of the signal.  If no initial value is provided, it defaults to `undefined`.
+ * @param options - Optional. An object containing signal options, including a custom equality function (`equal`).
+ *
+ * @returns A `MutableSignal` instance.
+ *
+ * @example
+ * // Create a mutable signal with an initial value:
+ * const mySignal = mutable({ count: 0 }) // MutableSignal<{ count: number }>;
+ *
+ * // Create a mutable signal with no initial value (starts as undefined):
+ * const = mutable<number>(); // MutableSignal<number | undefined>
+ *
+ * // Create a mutable signal with a custom equality function:
+ * const myCustomSignal = mutable({ a: 1 }, { equal: (a, b) => a.a === b.a });
+ */
+export function mutable<T>(): MutableSignal<T | undefined>;
+export function mutable<T>(initial: T): MutableSignal<T>;
+export function mutable<T>(
+  initial: T,
+  opt?: CreateSignalOptions<T>,
+): MutableSignal<T>;
+
+export function mutable<T>(
+  initial?: T,
+  opt?: CreateSignalOptions<T>,
+): MutableSignal<T> {
+  const baseEqual = opt?.equal ?? is;
+  let trigger = false;
+
+  const equal: ValueEqualityFn<T | undefined> = (a, b) => {
+    if (trigger) return false;
+    return baseEqual(a, b);
   };
-  
-  export function mutable<T>(): MutableSignal<T | undefined>;
-  export function mutable<T>(initial: T): MutableSignal<T>;
-  export function mutable<T>(
-    initial: T,
-    opt?: CreateSignalOptions<T>
-  ): MutableSignal<T>;
-  
-  export function mutable<T>(
-    initial?: T,
-    opt?: CreateSignalOptions<T>
-  ): MutableSignal<T> {
-    const baseEqual = opt?.equal ?? is;
-    let trigger = false;
-  
-    const equal: ValueEqualityFn<T | undefined> = (a, b) => {
-      if (trigger) return false;
-      if (!a && !b) return true;
-      if (!a || !b) return false;
-      return baseEqual(a, b);
-    };
-  
-    const sig = signal<T | undefined>(initial, {
-      ...opt,
-      equal,
-    }) as MutableSignal<T>;
 
-    const internalUpdate = sig.update;
-  
-    sig.mutate = (updater) => {
-      trigger = true;
-      internalUpdate(updater);
-      trigger = false;
-    };
+  const sig = signal<T | undefined>(initial, {
+    ...opt,
+    equal,
+  }) as MutableSignal<T>;
 
-    sig.inline = (updater) => {
-      sig.mutate((prev) => {
-        updater(prev);
-        return prev;
-      })
-    }
-  
-    return sig;
-  }
-  
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  export function isMutable<T = any>(
-    value: WritableSignal<T>
-  ): value is MutableSignal<T> {
-    return 'mutate' in value && typeof value.mutate === 'function';
-  }
-  
+  const internalUpdate = sig.update;
+
+  sig.mutate = (updater) => {
+    trigger = true;
+    internalUpdate(updater);
+    trigger = false;
+  };
+
+  sig.inline = (updater) => {
+    sig.mutate((prev) => {
+      updater(prev);
+      return prev;
+    });
+  };
+
+  return sig;
+}
+
+/**
+ * Type guard function to check if a given `WritableSignal` is a `MutableSignal`.  This is useful
+ * for situations where you need to conditionally use the `mutate` or `inline` methods.
+ *
+ * @typeParam T - The type of the signal's value (optional, defaults to `any`).
+ * @param value - The `WritableSignal` to check.
+ * @returns `true` if the signal is a `MutableSignal`, `false` otherwise.
+ *
+ * @example
+ * const mySignal = signal(0);
+ * const myMutableSignal = mutable(0);
+ *
+ * if (isMutable(mySignal)) {
+ *   mySignal.mutate(x => x + 1); // This would cause a type error, as mySignal is not a MutableSignal.
+ * }
+ *
+ * if (isMutable(myMutableSignal)) {
+ *   myMutableSignal.mutate(x => x + 1); // This is safe.
+ * }
+ */
+export function isMutable<T = any>(
+  value: WritableSignal<T>,
+): value is MutableSignal<T> {
+  return 'mutate' in value && typeof value.mutate === 'function';
+}
--- packages/primitives/src/lib/to-writable.ts
+++ packages/primitives/src/lib/to-writable.ts
@@ -1,5 +1,33 @@
 import { Signal, untracked, WritableSignal } from '@angular/core';
 
+/**
+ * Converts a read-only `Signal` into a `WritableSignal` by providing custom `set` and, optionally, `update` functions.
+ * This can be useful for creating controlled write access to a signal that is otherwise read-only.
+ *
+ * @typeParam T - The type of value held by the signal.
+ *
+ * @param signal - The read-only `Signal` to be made writable.
+ * @param set - A function that will be used to set the signal's value.  This function *must* handle
+ *              the actual update mechanism (e.g., updating a backing store, emitting an event, etc.).
+ * @param update - (Optional) A function that will be used to update the signal's value based on its
+ *                 previous value.  If not provided, a default `update` implementation is used that
+ *                 calls the provided `set` function with the result of the updater function. The
+ *                 default implementation uses `untracked` to avoid creating unnecessary dependencies
+ *                 within the updater function.
+ *
+ * @returns A `WritableSignal` that uses the provided `set` and `update` functions.  The `asReadonly`
+ *          method of the returned signal will still return the original read-only signal.
+ *
+ * @example
+ * // Basic usage: Making a read-only signal writable with a custom set function.
+ * const originalValue = signal({a: 0});
+ * const readOnlySignal = computed(() => originalValue().a);
+ * const writableSignal = toWritable(readOnlySignal, (newValue) => {
+ *  originalValue.update((prev) => { ...prev, a: newValue });
+ * });
+ *
+ * writableSignal.set(5); // sets value of originalValue.a to 5 & triggers all signals
+ */
 export function toWritable<T>(
   signal: Signal<T>,
   set: (value: T) => void,
