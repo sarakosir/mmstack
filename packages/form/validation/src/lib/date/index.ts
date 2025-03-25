516ebdfa0e22530a549e21bdc185aa69a5fb6ec7
miham <miham@marand.si>
2025-03-25 17:51:10 +0100

    fix: fix validation lib

--- packages/form/validation/src/lib/date/index.ts
+++ packages/form/validation/src/lib/date/index.ts
@@ -49,12 +49,13 @@ export function createDateValidators<TDate = Date>(
     isDate: createIsDateValidator(t.isDate, toDate),
   };
 
-  const toLabel = (d: Date | null) => (d ? formatDate(d, locale) : 'null');
+  const toLabel = (d: TDate | null) =>
+    d ? formatDate(toDate(d), locale) : 'null';
 
   return {
     ...base,
-    all: <TDate = Date>(opt: DateValidatorOptions) => {
-      const validators: Validator<Date | null>[] = [];
+    all: (opt: DateValidatorOptions) => {
+      const validators: Validator<TDate | null>[] = [];
 
       if (opt.required) validators.push(generalValidators.required());
 
@@ -62,56 +63,74 @@ export function createDateValidators<TDate = Date>(
 
       if (opt.mustBe !== undefined) {
         if (opt.mustBe === null)
-          validators.push(generalValidators.mustBeNull<Date | null>());
+          validators.push(generalValidators.mustBeNull<TDate | null>());
         else {
-          const d = toDate(opt.mustBe);
+          const d = toDate(opt.mustBe as TDate | string);
           const formatted = formatDate(d, locale);
           validators.push(
-            generalValidators.mustBe<Date | null>(d, formatted, (a, b) => {
-              if (!a && !b) return true;
-              if (!a || !b) return false;
+            generalValidators.mustBe<TDate | null>(
+              d as TDate,
+              formatted,
+              (a, b) => {
+                if (!a && !b) return true;
+                if (!a || !b) return false;
 
-              return toDate(a).getTime() === toDate(b).getTime();
-            }),
+                return toDate(a).getTime() === toDate(b).getTime();
+              },
+            ),
           );
         }
       }
 
       if (opt.not !== undefined) {
         if (opt.not === null)
-          validators.push(generalValidators.not<Date | null>(null));
+          validators.push(generalValidators.not<TDate | null>(null));
         else {
-          const d = toDate(opt.not);
+          const d = toDate(opt.not as TDate);
           const formatted = formatDate(d, locale);
           validators.push(
-            generalValidators.not<Date | null>(d, formatted, (a, b) => {
-              if (!a && !b) return true;
-              if (!a || !b) return false;
+            generalValidators.not<TDate | null>(
+              d as TDate,
+              formatted,
+              (a, b) => {
+                if (!a && !b) return true;
+                if (!a || !b) return false;
 
-              return toDate(a).getTime() === toDate(b).getTime();
-            }),
+                return toDate(a).getTime() === toDate(b).getTime();
+              },
+            ),
           );
         }
       }
 
-      if (opt.min !== undefined) validators.push(base.min(opt.min));
-      if (opt.max !== undefined) validators.push(base.max(opt.max));
+      if (opt.min !== undefined) validators.push(base.min(opt.min as TDate));
+      if (opt.max !== undefined) validators.push(base.max(opt.max as TDate));
 
       if (opt.oneOf) {
-        const dates = opt.oneOf.map((d) => (d ? toDate(d) : null));
+        const dates = opt.oneOf.map((d) => (d ? toDate(d as TDate) : null));
 
         validators.push(
-          generalValidators.oneOf<Date | null>(dates, toLabel, (a) =>
-            a === null ? 'null' : a.getTime().toString(),
+          generalValidators.oneOf<TDate | null>(
+            dates as TDate[],
+            toLabel,
+            (a) =>
+              a === null || a === undefined
+                ? 'null'
+                : toDate(a).getTime().toString(),
           ),
         );
       }
 
       if (opt.notOneOf) {
-        const dates = opt.notOneOf.map((d) => (d ? toDate(d) : null));
+        const dates = opt.notOneOf.map((d) => (d ? toDate(d as TDate) : null));
         validators.push(
-          generalValidators.notOneOf<Date | null>(dates, toLabel, (a) =>
-            a === null ? 'null' : a.getTime().toString(),
+          generalValidators.notOneOf<TDate | null>(
+            dates as TDate[],
+            toLabel,
+            (a) =>
+              a === null || a === undefined
+                ? 'null'
+                : toDate(a).getTime().toString(),
           ),
         );
       }
